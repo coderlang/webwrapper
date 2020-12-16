c115c0bfa28e268337d42ba638c1718db87b8ead
coderlang <coderlang@foxmail.com>
2020-12-16 09:22:39 +0800

    发布SessionStorage

--- src/WebDBManager.ts
+++ src/WebDBManager.ts
@@ -21,6 +21,27 @@ class WebLocalStorage implements Storage {
   }
 }
 
+class WebSessionStorage implements Storage {
+  async get(key: string): Promise<[string, Error | null]> {
+    let ret = sessionStorage.getItem(key);
+    if (ret === null) {
+      return Promise.resolve(["", new Error("key not found")]);
+    }
+
+    return [ret, null]
+  }
+
+  async remove(key: string): Promise<Error | null> {
+    sessionStorage.removeItem(key);
+    return Promise.resolve(null);
+  }
+
+  async set(key: string, value: string): Promise<Error | null> {
+    sessionStorage.setItem(key, value);
+    return Promise.resolve(null);
+  }
+}
+
 export class WebDBManager extends DBManager {
   protected newSessionStorage():Storage {
     return new WebLocalStorage();
