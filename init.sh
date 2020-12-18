2aadff15e9ab6ed272220998f5e8947532e34769
coderlang <coderlang@foxmail.com>
2020-12-18 16:41:36 +0800

    更新init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-git submodule init
-git submodule update
-cd 3rd_party && git checkout master && cd ..
-node 3rd_party/script/cmd/init.js
+./3rd_party/script/update.submodule.sh
+node ./3rd_party/script/cmd/init.js
